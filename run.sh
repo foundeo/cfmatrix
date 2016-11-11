38cb1fc16c38c27d0bb03d51aa48eded86c65464
Pete Freitag <pete@foundeo.com>
2016-11-11 14:27:46 -0500

    Update the runner shell script to output when  it fails

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-curl --fail "http://127.0.0.1:8123$TEST_RUNNER_URI"
+#execute the test runner and capture the status code
+echo "Running: http://127.0.0.1:8123$TEST_RUNNER_URI"
+http_code=$(curl -s -o /tmp/result.txt -w '%{http_code}' http://127.0.0.1:8123$TEST_RUNNER_URI;)
+echo "Finished with Status: $http_code"
+#output the result
+cat /tmp/result.txt
+if [[ $http_code -eq 200 ]]; then
+    exit 0
+fi
 
+#fail if status code is not 200
+exit 1
