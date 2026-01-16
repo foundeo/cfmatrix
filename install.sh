c87b045e7bc7ad28146644fee229452cf75d8311
Pete Freitag <pete@foundeo.com>
2026-01-16 13:42:01 -0500

    boxlang support

--- install.sh
+++ install.sh
@@ -5,8 +5,18 @@ curl -SL -o box.zip https://www.ortussolutions.com/parent/download/commandbox/ty
 ls -lah | grep box.zip
 unzip box.zip -d cfmatrix/bin/ && rm -v box.zip
 
+#for boxlang install bx compat 
+if [[ "$CFENGINE" == *"boxlang"* ]];then
+    cfmatrix/bin/box install bx-compat-cfml
+fi
+
+#if boxlang and no javaversion set to 21
+if [[ "$CFENGINE" == *"boxlang"* ]] && [[ -z "$JAVAVERSION" ]]; then
+    export JAVAVERSION="openjdk21"
+fi
+
 #run the recipie
-cfmatrix/bin/box recipe recipeFile=cfmatrix/ci.boxr CFENGINE=$CFENGINE
+cfmatrix/bin/box recipe recipeFile=cfmatrix/ci.boxr CFENGINE=$CFENGINE JAVAVERSION=$JAVAVERSION
 
 # if box.json installed a command the server start may fail
 # because commandbox reloads shell, so check if port is listening
@@ -17,5 +27,5 @@ if [[ $(cat "/tmp/ports") =~ ":8123" ]]; then
     cat /tmp/ports
 else
     echo "Server Not Started, attempting to start"
-    cfmatrix/bin/box server start cfengine=$CFENGINE port=8123 JVMArgs="-Djava.security.egd=file:/dev/./urandom" openbrowser=false
+    cfmatrix/bin/box server start cfengine=$CFENGINE port=8123 JVMArgs="-Djava.security.egd=file:/dev/./urandom" openbrowser=false javaVersion=$JAVAVERSION
 fi
